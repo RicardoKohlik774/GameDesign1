b7c84d471d99ab7889c40de016573ed86c9176c8
RicardoKohlik744 <kohlik@spsejecna.cz>
2025-02-26 11:41:03 +0100

    initial commit

--- src/World.java
+++ src/World.java
@@ -1,5 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class World {
 
 
+
+
+
+    public Boolean mapLoad() {
+        try (BufferedReader br = new BufferedReader(new FileReader("src\\Mapa.txt"))) {
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                   //do
+            }
+           //output
+        } catch (Exception e) {
+                  return false;
+        }
+        return true;
+    }
 
 }
